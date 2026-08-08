2cd29c7d10fb431cbe131a685599691f68ebf693
Spawn <moises.elias1510@gmail.com>
2026-08-08 10:16:06 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbz5qRP_9oyThAQITigmGWh-4-NsoVyaXQhRqdKqDX_WGDJvg3OgBV0VNGp1zQMXrc2qDA/exec";
+
 const BACKGROUND_LOGIN = "imagens/fundo-login.jpg";
-const BACKGROUND_HER = "imagens/fundo-dela.jpg";
-const BACKGROUND_HIM = "imagens/fundo-meu.jpg";
+const BACKGROUND_HER   = "imagens/fundo-dela.jpg";
+const BACKGROUND_HIM   = "imagens/fundo-meu.jpg";
